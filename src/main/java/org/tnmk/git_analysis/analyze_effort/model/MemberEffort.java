0d07dad26c543754d48f8bf22147dbf630625bc9
Khoi Tran <khoi.tnm@gmail.com>
2024-06-07 13:39:08 -0400

    Calculate avgChangedFiles per commit.

--- src/main/java/org/tnmk/git_analysis/analyze_effort/model/MemberEffort.java
+++ src/main/java/org/tnmk/git_analysis/analyze_effort/model/MemberEffort.java
@@ -2,17 +2,30 @@ package org.tnmk.git_analysis.analyze_effort.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class MemberEffort {
   private final String name;
   private int commits;
+  private int totalChangedFiles;
   private int pullRequests;
 
   public MemberEffort(String name) {
     this.name = name;
+  }
+
+  public String toString() {
+    return """
+      %s, commits: %s, avgChangedFiles: %.02f, totalChangedFiles: %s
+      """.formatted(name, commits, getAvgChangedFilesPerCommit(), totalChangedFiles);
+  }
+
+  public double getAvgChangedFilesPerCommit() {
+    return totalChangedFiles / (double) commits;
+  }
+
+  public void addChangedFilesCount(int changedFilesCount) {
+    totalChangedFiles += changedFilesCount;
   }
 }
