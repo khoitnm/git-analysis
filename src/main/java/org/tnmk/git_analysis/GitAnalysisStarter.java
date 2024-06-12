712558e863d551c48b56bf1155d26f3957684a7b
Khoi Tran <khoi.tnm@gmail.com>
2024-06-12 11:54:30 -0400

    Config date range in properties file.

--- src/main/java/org/tnmk/git_analysis/GitAnalysisStarter.java
+++ src/main/java/org/tnmk/git_analysis/GitAnalysisStarter.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.tnmk.git_analysis.analyze_effort.GitFoldersAnalyzer;
+import org.tnmk.git_analysis.config.GitAnalysisDateRangeProperties;
 import org.tnmk.git_analysis.config.GitFolderProperties;
 
 import java.io.IOException;
@@ -18,11 +19,13 @@ public class GitAnalysisStarter {
   private static final int ANALYZE_IN_WEEKS = 2;
   private final GitFolderProperties gitFolderProperties;
   private final GitFoldersAnalyzer gitFoldersAnalyzer;
+  private final GitAnalysisDateRangeProperties gitAnalysisDateRangeProperties;
 
   @EventListener(ApplicationStartedEvent.class)
   public void start() throws GitAPIException, IOException, JSchException {
-    LocalDateTime startTimeToAnalyze = LocalDateTime.now().minusWeeks(ANALYZE_IN_WEEKS);
-    LocalDateTime endTimeToAnalyze = LocalDateTime.now();
+
+    LocalDateTime startTimeToAnalyze = gitAnalysisDateRangeProperties.getStartDate();
+    LocalDateTime endTimeToAnalyze = gitAnalysisDateRangeProperties.getEndDate();
 //    LocalDateTime startTimeToAnalyze = LocalDateTime.of(2024, 1, 1, 0, 0);
     gitFoldersAnalyzer.analyzeManyRepos(startTimeToAnalyze, endTimeToAnalyze, gitFolderProperties.getPaths(), true);
   }
---
+++ src/main/java/org/tnmk/git_analysis/config/GitAnalysisDateRangeProperties.java
@@ -0,0 +1,34 @@
+package org.tnmk.git_analysis.config;
+
+import jakarta.annotation.Nullable;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@ConfigurationProperties(prefix = "git-analysis.date-range")
+@Setter
+public class GitAnalysisDateRangeProperties {
+  public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+  private String startDate;
+  @Nullable
+  private String endDate;
+
+  public LocalDateTime getStartDate() {
+    LocalDate localDate = LocalDate.parse(startDate, dateTimeFormatter);
+    return localDate.atStartOfDay();
+  }
+
+  public LocalDateTime getEndDate() {
+    if (StringUtils.isEmpty(endDate)) {
+      return LocalDateTime.now();
+    }
+    LocalDate localDate = LocalDate.parse(endDate, dateTimeFormatter);
+    return localDate.atStartOfDay();
+  }
+}
