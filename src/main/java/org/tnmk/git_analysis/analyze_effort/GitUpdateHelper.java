bfe467576e47800b4e8f78035b7c383bbe084480
Khoi Tran <khoi.tnm@gmail.com>
2024-10-24 10:55:22 -0400

    Update latest code when `fetch` is enabled.

--- src/main/java/org/tnmk/git_analysis/analyze_effort/GitUpdateHelper.java
+++ src/main/java/org/tnmk/git_analysis/analyze_effort/GitUpdateHelper.java
@@ -4,7 +4,6 @@ import com.jcraft.jsch.JSchException;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.tnmk.git_analysis.git_connection.GitSshHelper;
 
@@ -25,17 +24,11 @@ public class GitUpdateHelper {
       .call();
     log.info("Fetched {}!", repoPath);
 
-
-    // Merge the first existing branch from the possible branches list into the current branch
-    for (String branchName : possibleMainBranches) {
-      Ref branchRef = repository.findRef(branchName);
-      if (branchRef != null) {
-        git.merge()
-          .include(branchRef)
-          .call();
-        log.info("Update '" + branchName + "' branch with the latest changes from " + branchRef.getName());
-        break;
-      }
-    }
+    // Update the current branch with the latest changes from Git remote server.
+    git.pull()
+      .setTransportConfigCallback(GitSshHelper.createTransportConfigCallback())
+      .setTimeout(60)
+      .call();
+    log.info("Merged latest changes into branch: {}", repository.getBranch());
   }
 }
